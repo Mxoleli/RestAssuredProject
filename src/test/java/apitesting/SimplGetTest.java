48f2298a3fbbc4d06e13b247ba3e9313177db374
mxoleli.cwati <Soyama@654321>
2019-04-15 14:56:38 +0200

    Rest Assured request

---
+++ src/test/java/apitesting/SimplGetTest.java
@@ -0,0 +1,26 @@
+package apitesting;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class SimplGetTest {
+
+	@Test
+	public void GetWeatherDetails() {
+		
+		
+		RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+		
+		RequestSpecification httpRequest = RestAssured.given();
+		
+		Response response = httpRequest.request(Method.GET,"/Hyderabad");
+		
+		String responseBody = response.getBody().asString();
+		System.out.println("Response Body is: =>" + responseBody);
+	}
+
+}
